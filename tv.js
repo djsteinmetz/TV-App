a141c865b996f6ac2d8aa8afef30d9fd363b13b1
Ansel Adams <dj@volaciousbrands.com>
2018-07-17 09:22:04 -0500

    test combining functions to one

--- tv.js
+++ tv.js
@@ -1,6 +1,6 @@
 var request = require("request"), fs = require("fs");
 var TV = function() {
-  this.findShow = function(show) {
+  this.findShow = show => {
     var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
     request(URL, function(error, response, body) {
       if (!error && response.statusCode === 200) {
@@ -14,6 +14,9 @@ var TV = function() {
     });
   };
   this.findActor = function(actor) {
+    //test
+    //this.searchFunction("search", "people", actor, act[0].person.name , "\nBirthday: ", act[0].person.birthday , "\nGender: " , act[0].person.gender , "\nCountry of Birth: ", act[0].person.country.name , "\nURL: ", act[0].person.url)
+    
     var URL = "http://api.tvmaze.com/search/people?q=" + actor
     request(URL, function(error, response, body) {
       if (!error && response.statusCode === 200) {
@@ -26,5 +29,18 @@ var TV = function() {
       };
     });
   };
+  this.searchFunction = function(search, queryType, query, log1Val, log2, log2Val, log3, log3Val, log4, log4Val, log5, log5Val) {
+    var URL = "http://api.tvmaze.com/" + search + "/" + queryType + "?q=" + query;
+    request(URL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var movAct = JSON.parse(body);
+        let logs = "Name: " + log1Val + log2 + log2Val + log3 + log3Val + log4 + log4Val + log5 + log5Val + "\n\n"
+        fs.appendFile("log.txt", logs, function(err) {
+          if (err) throw err;
+        });
+        console.log(logs)
+      };
+    });
+  }
 };
 module.exports = TV;
